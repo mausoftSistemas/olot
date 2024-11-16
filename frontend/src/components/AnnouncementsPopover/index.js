c54adddae344a4d36fe45a3acd6684622575b141
mausoftSistemas <113848246+mausoftSistemas@users.noreply.github.com>
2024-11-16 10:00:47 -0300

    Add files via upload

--- frontend/src/components/AnnouncementsPopover/index.js
+++ frontend/src/components/AnnouncementsPopover/index.js
@@ -72,7 +72,7 @@ function AnnouncementDialog({ announcement, open, handleClose }) {
       </DialogContent>
       <DialogActions>
         <Button onClick={() => handleClose()} color="primary" autoFocus>
-          Fechar
+          Cerrar
         </Button>
       </DialogActions>
     </Dialog>
